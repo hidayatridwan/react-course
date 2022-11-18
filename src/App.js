3cd7205d8ee59b479e05c981864e57f55bb97c1a
hidayatridwan <ridwan.nurulhidayat@gmail.com>
2022-11-18 15:06:22 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Learn NodeJS" />
+      <Todo text="Learn Mysql" />
+    </div>
+  );
+}
+
+export default App;
