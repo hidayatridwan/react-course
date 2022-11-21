85273d5ea4cb6ce4fece59224d1f3f0e23a64824
hidayatridwan <ridwan.nurulhidayat@gmail.com>
2022-11-21 09:46:36 +0700

    initial step-2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import Todo from "./components/Todo";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Learn NodeJS" />
-      <Todo text="Learn Mysql" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
