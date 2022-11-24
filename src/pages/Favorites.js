ec73d2238ad58e50b09ec093e09267d449444154
hidayatridwan <ridwan.nurulhidayat@gmail.com>
2022-11-24 10:49:40 +0700

    finish projek meetups

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,20 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 function FavoritesPage() {
-  return <div>Favorites Page</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {favoritesCtx.totalFavorites === 0 ? (
+        <p>You got no favorites yet, Adding some?</p>
+      ) : (
+        <MeetupList meetups={favoritesCtx.favorites} />
+      )}
+    </section>
+  );
 }
 
 export default FavoritesPage;
